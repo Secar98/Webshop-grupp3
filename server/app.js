705d591c2bc4ce6f5849de4cce9bf0da75534356
Sebastian Carlson <sebcar98@gmail.com>
2021-10-11 18:44:55 +0200

    add mongoose and connect server to database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -13,6 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+const uri = process.env.DB_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.log(error));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
