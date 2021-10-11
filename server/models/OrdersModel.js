042c06951f530c213db070c275a87f0bb0e1049a
Sebastian Carlson <sebcar98@gmail.com>
2021-10-11 20:24:45 +0200

    add OrdersModel

---
+++ server/models/OrdersModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    requied: true,
+  },
+  products: [
+    {
+      amount: {
+        type: Number,
+        required: true,
+      },
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  shipping: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  deliveryAdress: {
+    postalCode: { type: Number, required: true },
+    streetAdress: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const UserModel = require("../models/UserModel");
+const UsersModel = require("../models/UsersModel");
 
-const ProductModel = require("../models/ProductsModel");
+const OrdersModel = require("../models/OrdersModel");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
+  const order = new OrdersModel({
+    user: "6164707600930933d55f9296",
+    products: [
+      {
+        product: "616476a066f9d18e67689c51",
+        amount: 20,
+      },
+    ],
+    shipping: 25,
+    totalPrice: 25,
+    deliveryAdress: {
+      postalCode: 12345,
+      streetAdress: "test",
+      city: "test",
+    },
+    status: "test",
+  });
+
+  order.save();
+
   res.send("respond with a resource");
 });
 
