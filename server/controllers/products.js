26d1d3e4675cb203d383fa9a1072ef5a658d6de3
Nina <>
2021-10-12 11:04:57 +0200

    add functionality for getting data from db

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var Products = require('../models/ProductsModel');
 
-const getAllProducts = (req, res) => {
-    res.json({text: "hello2"})
+const getAllProducts = async (req, res) => {
+    try {
+        const allProducts = await Products.find()
+        res.status(200).json(allProducts)
+    }
+    catch (err) {
+        res.status(404).json({message: err.message})
+    }
 }
 
 module.exports = {getAllProducts};
