5cd4394ceb46eb49806f057551e3c1edd05484c9
Nina <>
2021-10-13 16:38:51 +0200

    styling of product list page, start adding sorting functionality

--- client/src/pages/AllProductsPage.jsx
+++ client/src/pages/AllProductsPage.jsx
@@ -16,20 +16,40 @@ export default function AllProductsPage() {
     fetchData()
   }, [])
 
+  function getProductsGender(gender) {
+    //input men or women
+    //map through all products, and filter by input / category
+    //save data somewhere? 
+    console.log(gender);
+  }
+
   return (
     <>
-      <div className="row">
+      
+      <div>
 
         {!productsData && <h1>Loading...</h1>}
 
-        {productsData && productsData.map((product, index) => {
-          return (
-            <div className="col-md-4">
-              <ProductListItem key={index} product={product}/>
-            </div>
-            
-          )
-        })}
+        {productsData && 
+        
+        <>
+          <h2 onClick={()=>getProductsGender("women")}>women</h2>
+          <h2 onClick={()=>getProductsGender("men")}>men</h2>
+          <div className="row">{productsData.map((product, index) => {
+            return (
+              <>
+                <div className="col-md-4">
+                  <ProductListItem key={index} product={product}/>
+                </div>
+                {/* if woman -> render woman products 
+                  if men -> render men products
+              */}
+              </>
+            )
+          })}
+          </div>
+        </>
+        }
       </div>
     </>
   )
