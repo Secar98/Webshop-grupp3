68296b80710d29d0dd78405839d6fd4e2286ed3f
amandaheideman <hedieman.amanda@gmail.com>
2021-10-13 10:17:16 +0200

    checkout route

---
+++ server/controllers/checkout.js
@@ -0,0 +1,33 @@
+const Products = require("../models/ProductsModel");
+
+//Hämta storedCart från localstorage
+
+//loopa igenom array, för varje id sök i databasen
+//pusha in i ny array namn, pris, antal och totalpris
+
+const getCart = async (req, res) => {
+  try {
+    let storedCart = []; //hämtas från localstorage
+    
+    let cart = [];
+
+    for (i = 0; i < storedCart.length; i++) {
+      const id = storedCart[i].id;
+      const product = await Products.findById(id);
+      let total = product.price * storedCart[i].amount;
+
+      cart.push({
+        title: product.title,
+        price: product.price,
+        amount: storedCart[i].amount,
+        total: total,
+      });
+    }
+    res.status(200).json(cart);
+
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+module.exports = {getCart};
