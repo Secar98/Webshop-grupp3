70a8d24345f629158a5959075f64a51214bf4567
Georgios <test@example.com>
2021-10-13 10:57:10 +0200

    minor fixes

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var {getAllOrders,getDetailOrder} = require('../controllers/orders')
+var {verify} = require('../utils/auth') 
 
 
-router.get('/', getAllOrders);
-router.get('/:id', getDetailOrder);
+
+router.get('/', verify, getAllOrders);
+router.get('/:id', verify, getDetailOrder);
 
 module.exports = router;
