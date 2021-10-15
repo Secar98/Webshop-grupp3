987e21ae8f336b58d5023435d70d1c03fa9d9b7e
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-15 20:09:23 +0200

    Add react-icons dependency

--- client/src/Pages/UserProfilePage.jsx
+++ client/src/Pages/UserProfilePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaUser } from 'react-icons/fa';
 
 export default function UserProfilePage() {
+
+    // Get user data- check if a user is present fetch data from /api/user here or get it from the context if saved there
     return (
-        <div>
-            <h1>This is User Profile Page</h1>
+        <div className="row">
+            
+            <div className="col-md-3 border rounded">
+                <div>
+                    <FaUser style={{fontSize: '7rem'}} />
+                    <h2>Name</h2>
+                </div>
+
+
+            </div>
         </div>
     )
 }
