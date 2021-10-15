2b05f5dcec6d2780043f168cf574105e1db23c24
amandaheideman <hedieman.amanda@gmail.com>
2021-10-15 10:34:00 +0200

    add product details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-//import react from 'react'
+import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import LoginPage from './Pages/LoginPage';
 import RegisterPage from './Pages/RegisterPage';
-import AllProductsPage from './pages/AllProductsPage';
+import AllProductsPage from './Pages/AllProductsPage';
 import {UserContext} from './context/userContext';
 import {useState} from 'react';
+import ProductDetailsPage from './Pages/ProductDetailsPage';
 
 function App() {
   const [newUser, setNewUser] = useState(Boolean)
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path="/login" component={LoginPage}/>  
           <Route path="/register" component={RegisterPage}/>  
+          <Route path="/:id" component={ProductDetailsPage}/>
           <Route path="/"><AllProductsPage/></Route>
         </Switch>
       </UserContext.Provider>
