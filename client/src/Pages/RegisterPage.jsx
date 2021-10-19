09398fdf43a1f883bde9c21bdb02c043896c8917
Sebastian Carlson <sebcar98@gmail.com>
2021-10-19 14:00:08 +0200

    update usermodel and changed registration form

---
+++ client/src/Pages/RegisterPage.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import FetchKit from "../utils/fetchKit";
+import { UserContext } from "../context/userContext";
+
+export default function RegisterPage() {
+  const history = useHistory();
+  const { setNewUser } = useContext(UserContext);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      fullName: e.target[0].value,
+      email: e.target[1].value,
+      password: e.target[2].value,
+      phoneNumber: e.target[3].value,
+      deliveryAdress: {
+        postalCode: e.target[4].value,
+        streetAdress: e.target[5].value,
+        city: e.target[6].value,
+      },
+    };
+
+    FetchKit.registerFetch(formData)
+      .then((res) => res.json())
+      .then((item) => {
+        setNewUser(true);
+        history.push("/login");
+      });
+  };
+
+  return (
+    <div>
+      <form method="POST" onSubmit={handleOnSubmit}>
+        <input name="fullName" type="text" placeholder="Fullname" />
+        <input name="email" type="email" placeholder="email" />
+        <input name="password" type="password" placeholder="password" />
+        <input name="phoneNumber" type="tel" placeholder="Phone" />
+        <input name="postalCode" type="number" placeholder="Postalcode" />
+        <input name="streetAdress" type="text" placeholder="Streetadress" />
+        <input name="city" type="text" placeholder="City" />
+        <input type="submit" value="register" />
+      </form>
+    </div>
+  );
+}
