711f140cff49482edaa21094c56bf4aa42c7cb8b
Georgios <ulidin@gmail.com>
2021-10-21 13:53:51 +0200

    try to fix check out page

--- client/src/Pages/CheckoutPage.jsx
+++ client/src/Pages/CheckoutPage.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, ListGroup } from "react-bootstrap";
 import Navigation from "../components/Navigation";
 
 const CheckoutPage = () => {
-  //   const fetchCart = () => {
 
-  //   };
+  const [productsData, setProductsData] = useState(null);
+
+  const fetchData = () => {
+    const url = "http://localhost:3000/api/products/";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setProductsData(data);
+      });
+  };
 
   const countCart = () => {
     if (localStorage.getItem("Cart")) {
@@ -15,18 +23,51 @@ const CheckoutPage = () => {
       for (const num of cart) {
         counts[num] = counts[num] ? counts[num] + 1 : 1;
       }
+      console.log("count cart");
       console.log(Object.entries(counts));
+
+      return(Object.entries(counts));
     }
   };
 
   useEffect(() => {
+    fetchData();
     countCart();
   }, []);
+
+  const cart = JSON.parse(localStorage.getItem("Cart"));
+
+  if(productsData){
+    countCart.map(Item => {
+      const testProd = productsData.find(product => 
+        product._id === Item)
+        console.log(testProd);
+      });
+  }
 
   return (
     <Container>
       <Navigation />
-      <h1>Du är på checkoutPage</h1>
+      <h1 class="mt-5">Du är på checkoutPage</h1>
+
+    {/* {productsData && */}
+    {/* cart.map(Item => {
+      const testProd = productsData.find(product => 
+        product._id === Item)
+        return(
+          <ListGroup class="p-2 bg-light border mt-5" as="ol" numbered>
+            <ListGroup.Item as="li">{testProd.title}</ListGroup.Item>
+            <ListGroup.Item as="li">{testProd.price}</ListGroup.Item>
+            <ListGroup.Item as="li">{testProd}</ListGroup.Item>
+            <ListGroup.Item as="li">{testProd}</ListGroup.Item>
+          </ListGroup>
+        )
+      }) */}
+    }
+
+      {/* <ListGroup class="p-2 bg-light border mt-5" as="ol" numbered>
+        <ListGroup.Item as="li">{localStorage.getItem("Cart")}</ListGroup.Item>
+      </ListGroup> */}
     </Container>
   );
 };
