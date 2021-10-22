9fe7edf69d52251715b434f399c5cd40cb4b499f
Sebastian Carlson <sebcar98@gmail.com>
2021-10-22 11:55:47 +0200

    new fetch function

--- client/src/Pages/CheckoutPage.jsx
+++ client/src/Pages/CheckoutPage.jsx
@@ -3,16 +3,30 @@ import { Container, ListGroup } from "react-bootstrap";
 import Navigation from "../components/Navigation";
 
 const CheckoutPage = () => {
-
   const [productsData, setProductsData] = useState(null);
 
   const fetchData = () => {
-    const url = "http://localhost:3000/api/products/";
-    fetch(url)
+    const items = countCart();
+    const products = items.map((item) => {
+      return item[0];
+    });
+
+    const body = {
+      products: products,
+    };
+
+    fetch("http://localhost:3000/api/products/checkout", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
       .then((res) => res.json())
       .then((data) => {
         setProductsData(data);
-      });
+      })
+      .catch((err) => console.log(err));
   };
 
   const countCart = () => {
@@ -23,7 +37,7 @@ const CheckoutPage = () => {
       for (const num of cart) {
         counts[num] = counts[num] ? counts[num] + 1 : 1;
       }
-      
+
       return Object.entries(counts);
     }
   };
@@ -32,36 +46,36 @@ const CheckoutPage = () => {
     fetchData();
     countCart();
   }, []);
-  const cartArray = countCart()
-  
+  const cartArray = countCart();
+
   return (
     <Container>
       <Navigation />
       <h1 class="mt-5">Du är på checkoutPage</h1>
 
-      {
-        (productsData && cartArray) &&
-        cartArray.map((item, index)=> {
-          const testProd = productsData.find(product => product._id === item[0])
-          const sum = Number(testProd.price*item[1])
-          return (<>
-            <ListGroup class="p-2 bg-light border mt-5" as="ol" numbered>
-              <ListGroup.Item as="li">
-                <p>Title: {testProd.title}</p>
-                <p>Price: {testProd.price}</p>
-                <p>Amount: {item[1]}</p>
-                <b class="bald">sum: {sum}</b>
-              </ListGroup.Item>
-            </ListGroup>
+      {productsData &&
+        cartArray &&
+        cartArray.map((item, index) => {
+          const testProd = productsData.find(
+            (product) => product._id === item[0]
+          );
+          const sum = Number(testProd.price * item[1]);
+          return (
+            <>
+              <ListGroup class="p-2 bg-light border mt-5" as="ol" numbered>
+                <ListGroup.Item as="li">
+                  <p>Title: {testProd.title}</p>
+                  <p>Price: {testProd.price}</p>
+                  <p>Amount: {item[1]}</p>
+                  <b class="bald">sum: {sum}</b>
+                </ListGroup.Item>
+              </ListGroup>
             </>
-          ) 
-        })
-      }
-    
+          );
+        })}
+
       <ListGroup class="p-2 bg-light border mt-5" as="ol" numbered>
-        <ListGroup.Item as="li">
-          
-        </ListGroup.Item>
+        <ListGroup.Item as="li"></ListGroup.Item>
       </ListGroup>
     </Container>
   );
