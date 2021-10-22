e9e4808086258efa638c6ab6888f58c4db0169fb
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-22 08:47:30 +0200

    fix: orders page to show order id

--- client/src/components/OrderItem.jsx
+++ client/src/components/OrderItem.jsx
@@ -1,25 +1,22 @@
 import React,{ useState } from 'react'
 
 export default function OrderItem({_id,status,totalPrice}) {
-    console.log(_id)
+
         const [orderItem,setOrderItem] = useState(false)
+        
     return (
         <div>
-            <h4 onClick = {()=>setOrderItem(!orderItem)}>Order Number</h4>
+            <h6 onClick = {()=>setOrderItem(!orderItem)}>Order Id : {_id}</h6>
             {orderItem && 
                 <table>
                 <tbody>
-                    <tr>
-                        <th>Order number</th>
-                        <th>{_id}</th>
-                    </tr>
                     <tr>
                         <th>status</th>
                         <th>{status}</th>
                     </tr>
                     <tr>
                         <th>Total Price</th>
-                        <th>{totalPrice}</th>
+                        <th>{totalPrice} SEK</th>
                     </tr>
                 </tbody>
             </table>
