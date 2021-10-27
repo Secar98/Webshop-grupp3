6a5b325920992245832a3df8067d477a72d35322
kevin basele <kevinbasele@hotmail.se>
2021-10-27 08:57:35 +0200

    removed som comments

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -9,9 +9,12 @@ class Auth {
         try{
 
             const validateToken = await FetchKit.validateJWTFetch(token)
-            if(validateToken.ok) return true
-            else return false
-            
+            if(validateToken.ok) {
+                return true
+            }
+            else {
+                return false
+            }
         }
         
         catch{
@@ -21,18 +24,5 @@ class Auth {
     }
     
 }
-
-/* const validateJWT = async () =>{
-    const token = localStorage.getItem("token")
-    const newToken = await FetchKit.validateJWTFetch(token)
-    console.log(newToken)
-    if(newToken){
-        localStorage.setItem("token", newToken)
-        return true
-    } 
-    else {
-        return false
-    }
-} */
 
 export default new Auth
