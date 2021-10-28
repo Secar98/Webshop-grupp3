01df90f428337463590dab6befbf713a05481186
kevin basele <kevinbasele@hotmail.se>
2021-10-28 10:26:09 +0200

    can now change amount of products in cart

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -27,35 +27,29 @@ const addOrder = async (req, res) => {
     const { products } = req.body
     const futureProduct = products.map(function (x) {
         return {
-            "amount": x[1],
-            "product": x[0],
+            "amount": x.amount,
+            "product": x.id,
         }
     })
 
-
     const user = req.user
     const newProduct = []
-    products.map(item => newProduct.push(item[0]))
+    products.map(item => newProduct.push(item.id))
     const userData = await User.findById(user)
 
     const productsData = await Products.find({ '_id': { $in: newProduct } })
     const shipping = 50;
-
     let total = shipping;
     products.map(item => {
-
-        const current = productsData.find(product => item[0] == product._id)
-        total += current.price * item[1]
+        const current = productsData.find(product => item.id == product._id)
+        total += current.price * item.amount
     })
     const { deliveryAddress } = userData
     const status = "not send";
 
     let newObj = { products: futureProduct, shipping, totalPrice: total, deliveryAddress, status, user }
-
     const newOrder = await new Orders(newObj);
-    console.log(futureProduct)
     newOrder.save()
-
         .then((order) => {
 
             res.status(201).json(order);
